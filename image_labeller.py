e0036727e2a3ca469596da34ef7bf479422a29d0
DhyeyDabhiMitti <165878126+DhyeyDabhiMitti@users.noreply.github.com>
2024-04-10 18:53:13 +0530

    changes_updated

--- image_labeller.py
+++ image_labeller.py
@@ -52,6 +52,7 @@ if 'data' not in st.session_state:
     st.session_state['data'] = pd.read_csv(response.get("Body"))
     st.session_state['data']['FieldPhot1hldr'].fillna('None')
     st.session_state['data']['FieldPhot2hldr'].fillna('None')
+print(df.columns)
 df = st.session_state['data']
 
 ## get the user and initiate the counter name as well as the counter ##
